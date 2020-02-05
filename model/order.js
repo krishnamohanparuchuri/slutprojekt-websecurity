be54a1a81d5d9ea21ac51121db3ae270d2771dbf
jenseneducation-krishna-paruchuri <paruchurikris@gmail.com>
2020-02-05 22:16:21 +0100

    order value uppdating in orders json

--- model/order.js
+++ model/order.js
@@ -1,15 +1,27 @@
 const dataStore= require('nedb-promise')
 const User = require('../model/user')
+const Product = require('../model/product')
 const orderDb = new dataStore({filename:'./dataBase/orderList.db', autoload: true})
 
 module.exports = {
   async create(body,userId){
+   
+    let itemprice = 0
+      for(let items=0; items< body.items.length;items++){
+       const itemlist = await Product.get(body.items[items])
+        itemprice += parseInt(itemlist.price)
+      }
+      console.log(itemprice)
+
+    
+    /* let productId = orderDb.find({_id:body.items}) */ 
+
       const newOrder = {
         owner:userId,
         timeStamp: Date.now(), // add server side
         status: 'inProcess', // done
         items: body.items,
-        orderValue: body.payment.total
+        orderValue: itemprice
 
     }
       
